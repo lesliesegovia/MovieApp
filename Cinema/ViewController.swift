56bd64787f13f144e4f6bf4fed6900d662312ec4
Leslie Segovia <segovialeslie1030@gmail.com>
2022-02-19 10:30:27 -0800

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Leslie Segovia on 2/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
